f2c1cdaabb36e5074368dfe195474faba1ac0dbb
Hamid Tlailia <tlhamid18@gmail.com>
2026-08-13 14:49:26 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// base: "./" ليعمل الموقع تحت أي مسار في GitHub Pages
-export default defineConfig({
-  base: "./",
-  plugins: [react()],
-  build: { outDir: "docs", emptyOutDir: true },
-});
